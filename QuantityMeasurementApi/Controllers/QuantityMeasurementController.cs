57e5e10f1af7b29825c85b3d977f8f66794b6c78
unknown <vishalpwaman123@gmail.com>
2020-07-17 06:00:30 -0700

    Added : Created API FOr Convert Value Unit And Store in Database

--- QuantityMeasurementApi/Controllers/QuantityMeasurementController.cs
+++ QuantityMeasurementApi/Controllers/QuantityMeasurementController.cs
@@ -12,9 +12,38 @@ namespace QuantityMeasurementApi.Controllers
     {
         private QuantityMeasurementInterfaceBusiness quantityMeasurementBusiness;
 
-        
 
-        
+        public QuantityMeasurementController(QuantityMeasurementInterfaceBusiness quantityMeasurementBusiness)
+        {
+            this.quantityMeasurementBusiness = quantityMeasurementBusiness;
+        }
+
+        [HttpPost]
+        public IActionResult Convert([FromBody] Quantity quantity)
+        {
+            try
+            {
+                var result = quantityMeasurementBusiness.Convert(quantity);
+                if (!result.Equals(null))
+                {
+                    bool success = true;
+                    var message = "Data Added Sucessfully";
+                    return this.Ok(new { success, message, data = result });
+                }
+                else
+                {
+                    bool success = false;
+                    var message = "Failed To Add Data";
+                    return this.BadRequest(new { success, message, data = result });
+                }
+            }
+            catch (Exception e)
+            {
+                bool success = false;
+                return BadRequest(new { success, message = e.Message });
+            }
+        }
+
 
     }
 }
