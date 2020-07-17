43f9953aee118d77fde32752a08f2ed40e32c538
unknown <vishalpwaman123@gmail.com>
2020-07-17 07:05:45 -0700

    Added : Testcase for Comparison unit and Store in  Database

---
+++ QuentityMeasurementTest/QuantityMeasurementCompareTest.cs
@@ -0,0 +1,419 @@
+﻿using BusinessLayer.Interface;
+using BusinessLayer.Services;
+using CommanLayer;
+using CommanLayer.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuantityMeasurementApi.Controllers;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace QuantityMeasurementConvertTest
+{
+    public class QuantityMeasurementCompareTest
+    {
+
+        QuantityMeasurementController quantityMeasurementController;
+        QuantityMeasurementInterfaceBusiness quantityMeasurementInterfaceBusiness;
+        QuantityMeasurementInterfaceRepository quantityMeasurementRepository;
+
+        public static DbContextOptions<ApplicationDbContext> Comparisons { get; }
+
+        public static string sqlConnectionString = "Data Source=DESKTOP-OF8D1IH;Initial Catalog=QuantityMeasurementDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        static QuantityMeasurementCompareTest()
+        {
+            Comparisons = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlServer(sqlConnectionString).Options;
+        }
+
+        public QuantityMeasurementCompareTest()
+        {
+
+            var dbContext = new ApplicationDbContext(Comparisons);
+            this.quantityMeasurementRepository = new QuantityMeasurementRepository(dbContext);
+            this.quantityMeasurementInterfaceBusiness = new QuantityMeasurementBusiness(this.quantityMeasurementRepository);
+            this.quantityMeasurementController = new QuantityMeasurementController(this.quantityMeasurementInterfaceBusiness);
+
+        }
+
+        [Fact]
+        public void Given2FeetToInchAnd24Inch_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "length",
+                    FirstValueUnit = "FeetToInch",
+                    FirstValue = 2,
+                    SecondValueUnit = "Inch",
+                    SecondValue = 24
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given1YardToInchAnd2FeetToInch_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "length",
+                    FirstValueUnit = "YardToInch",
+                    FirstValue = 1,
+                    SecondValueUnit = "FeetToInch",
+                    SecondValue = 2
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given1YardToInchAnd36Inch_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "length",
+                    FirstValueUnit = "YardToInch",
+                    FirstValue = 1,
+                    SecondValueUnit = "Inch",
+                    SecondValue = 36
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given14InchAnd2FeetToInch_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "length",
+                    FirstValueUnit = "Inch",
+                    FirstValue = 14,
+                    SecondValueUnit = "FeetToInch",
+                    SecondValue = 2
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given5CentimeterToInchValueAnd2InchValue_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "length",
+                    FirstValueUnit = "CentimeterToInchValue",
+                    FirstValue = 5,
+                    SecondValueUnit = "Inch",
+                    SecondValue = 2
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void GivenNullAnd2InchValue_WhenCompared_ReturnsBadResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "length",
+                    FirstValueUnit = null,
+                    FirstValue = 5,
+                    SecondValueUnit = "Inch",
+                    SecondValue = 2
+                };
+                var badResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<BadRequestObjectResult>(badResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given1GallonToLitreValueAnd3point78LitreValue_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "volume",
+                    FirstValueUnit = "GallonToLitre",
+                    FirstValue = 1,
+                    SecondValueUnit = "Litre",
+                    SecondValue = 3.78
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given100MiliLitreToLitreValueAnd10LitreValue_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "volume",
+                    FirstValueUnit = "MiliLitreToLitre",
+                    FirstValue = 10000,
+                    SecondValueUnit = "Litre",
+                    SecondValue = 10
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given10LitreValueAnd10LitreValue_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "volume",
+                    FirstValueUnit = "Litre",
+                    FirstValue = 10,
+                    SecondValueUnit = "Litre",
+                    SecondValue = 10
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void GivenNullValueAnd3point78LitreValue_WhenCompared_ReturnsBadResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "volume",
+                    FirstValueUnit = null,
+                    FirstValue = 1,
+                    SecondValueUnit = "Litre",
+                    SecondValue = 3.78
+                };
+                var badResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<BadRequestObjectResult>(badResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given1000GramsToKilogramValueAnd1KiloGram_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "weight",
+                    FirstValueUnit = "GramsToKilogram",
+                    FirstValue = 1000,
+                    SecondValueUnit = "kilogram",
+                    SecondValue = 10
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given1TonneToKilogramsValueAnd100KiloGram_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "weight",
+                    FirstValueUnit = "TonneToKilograms",
+                    FirstValue = 1,
+                    SecondValueUnit = "kilogram",
+                    SecondValue = 100
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given10kilogramValueAnd10KiloGram_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "weight",
+                    FirstValueUnit = "kilogram",
+                    FirstValue = 10,
+                    SecondValueUnit = "kilogram",
+                    SecondValue = 10
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void GivenNullAnd10KiloGram_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "weight",
+                    FirstValueUnit = null,
+                    FirstValue = 10,
+                    SecondValueUnit = "kilogram",
+                    SecondValue = 10
+                };
+                var badResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<BadRequestObjectResult>(badResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given22FahrenheitToCelsiusAnd10Celsius_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "temperature",
+                    FirstValueUnit = "FahrenheitToCelsius",
+                    FirstValue = 22,
+                    SecondValueUnit = "Celsius",
+                    SecondValue = 10
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void Given150CelsiusAnd150Celsius_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "temperature",
+                    FirstValueUnit = "Celsius",
+                    FirstValue = 150,
+                    SecondValueUnit = "Celsius",
+                    SecondValue = 150
+                };
+                var okResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<OkObjectResult>(okResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [Fact]
+        public void GivenNullAnd150Celsius_WhenCompared_ReturnsResult()
+        {
+            try
+            {
+                Compare result = new Compare
+                {
+                    MeasurementType = "temperature",
+                    FirstValueUnit = null,
+                    FirstValue = 150,
+                    SecondValueUnit = "Celsius",
+                    SecondValue = 150
+                };
+                var badResult = quantityMeasurementController.CompareAndAdd(result);
+                Assert.IsType<BadRequestObjectResult>(badResult);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+    }
+}
